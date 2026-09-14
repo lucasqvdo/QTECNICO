e1561da5458d76e4575aba564a282d6f0446db44
Lucas Quevedo <87128880+lucasqvdo@users.noreply.github.com>
2026-09-14 18:05:12 -0300

    fix: reload clients when PWA updates

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
 import { createRoot } from "react-dom/client";
+import { registerSW } from "virtual:pwa-register";
 import DeviceRouter from "./app/DeviceRouter";
 import "./styles/index.css";
+
+registerSW({ immediate: true });
 
 createRoot(document.getElementById("root")!).render(<DeviceRouter />);
